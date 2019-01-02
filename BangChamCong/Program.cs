901dd710023421ca5bfaa3feeb6f1d282916b897
purple_sunset <purple.sunshine.alone@gmail.com>
2019-01-02 17:22:33 +0700

    Rename Form1

--- BangChamCong/Program.cs
+++ BangChamCong/Program.cs
@@ -16,7 +16,7 @@ namespace BangChamCong
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
